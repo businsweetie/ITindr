46858ea54f67e683e0852b3241a32104adebbd58
businsweetie <72300436+businsweetie@users.noreply.github.com>
2022-04-08 14:09:14 +0200

    Create AboutYorSelfScreenViewController.swift

---
+++ ITindr/AboutYorSelfScreen/AboutYorSelfScreenViewController.swift
@@ -0,0 +1,66 @@
+//
+//  AboutYorSelfScreenViewController.swift
+//  ITindr
+//
+//  Created by user218932 on 4/8/22.
+//
+
+import UIKit
+
+class AboutYorSelfScreenViewController: UIViewController, TagListViewDelegate {
+
+    @IBOutlet weak var tagListView: TagListView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        tagListView.textFont = UIFont.systemFont(ofSize: 24)
+//        tagListView.alignment = .center // possible values are [.leading, .trailing, .left, .center, .right]
+//        tagListView.minWidth = 57
+//
+//        tagListView.addTag("TagListView")
+//        tagListView.addTags(["Add", "two", "tags"])
+//
+//        tagListView.insertTag("This should be the second tag", at: 1)
+//
+//        tagListView.setTitle("New Title", at: 6) // to replace the title a tag
+//
+//        tagListView.removeTag("meow") // all tags with title “meow” will be removed
+//        tagListView.removeAllTags()
+        
+//        tagListView.delegate = self
+//        tagListView.addTag("TagListView")
+//        tagListView.addTag("TEAChart")
+//        tagListView.addTag("To Be Removed")
+//        tagListView.addTag("To Be Removed")
+//        tagListView.addTag("Quark Shell")
+//        tagListView.removeTag("To Be Removed")
+//        tagListView.addTag("On tap will be removed").onTap = { [weak self] tagView in self?.tagListView.removeTagView(tagView)
+//        }
+//        tagListView.textFont = .systemFont(ofSize: 16)
+//        let tagView = tagListView.addTag("gray")
+//        tagView.tagBackgroundColor = UIColor.gray
+//        tagView.onTap = {
+//            tagView in print("Don’t tap me!")
+//        }
+//        tagListView.insertTag("This should be the third tag", at:2)
+    }
+    
+//    override func didReceiveMemoryWarning() {
+//            super.didReceiveMemoryWarning()
+//            // Dispose of any resources that can be recreated.
+//        }
+//
+//        // MARK: TagListViewDelegate
+//        func tagPressed(_ title: String, tagView: TagView, sender: TagListView) {
+//            print("Tag pressed: \(title), \(sender)")
+//            tagView.isSelected = !tagView.isSelected
+//        }
+//        
+//        func tagRemoveButtonPressed(_ title: String, tagView: TagView, sender: TagListView) {
+//            print("Tag Remove pressed: \(title), \(sender)")
+//            sender.removeTagView(tagView)
+//        }
+//    
+
+}
